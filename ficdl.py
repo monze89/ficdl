8714bd681c965c3cfa6622c4b7d6b3fa38b3445a
Karthikeya Viswanath <viswanathkarthikeya@gmail.com>
2014-01-19 02:07:58 -0100

    Added list functions

--- ficdl.py
+++ ficdl.py
@@ -3,6 +3,7 @@ import requests as re
 import sys
 from lxml import html
 import subprocess
+from optparse import OptionParser
 
 # Basics
 #
@@ -18,67 +19,91 @@ import subprocess
 # Step 4:  Use calibre to convert the file to required format
 #
 
-if len(sys.argv) < 2:
-    sys.stderr.write("Usage: ficdl.py <url> <type>[default is mobi]")
-    exit(0)
+fList = ''
+if '-f' in sys.argv:
+    fList = sys.argv[sys.argv.index('-f')+1]
+
+if '-d' in sys.argv:
+    dest = sys.argv[sys.argv.index('-d')+1]
+
+if '-u' in sys.argv:
+    url = sys.argv[sys.argv.index('-u')+1]
 else:
-    if len(sys.argv) == 3:
-        fType = sys.argv[2]
-        if fType in ['mobi','Mobi','MOBI']:
-            filType = 'mobi'
-        if fType in ['epub','Epub','EPUB']:
-            filType = 'epub'
-        if fType in ['pdf','Pdf','PDF']:
-            filType = 'pdf'
-    else:
+    if fList == '':
+        print "Please enter a story URL"
+        exit(0)
+
+if '-t' in sys.argv:
+    fType = sys.argv[sys.argv.index('-t')+1]
+
+    if fType in ['mobi','Mobi','MOBI']:
         filType = 'mobi'
-    storyUrl = sys.argv[1]
+    if fType in ['epub','Epub','EPUB']:
+        filType = 'epub'
+    if fType in ['pdf','Pdf','PDF']:
+        filType = 'pdf'
+else:
+    filType = 'mobi'
 
+def getStory(storyUrl):
+    # Find the story id
+    storyId = storyUrl[storyUrl.find('/s/')+3:storyUrl.find('/s/')+10]
 
-# Find the story id
-storyId = storyUrl[storyUrl.find('/s/')+3:storyUrl.find('/s/')+10]
-
-# Get number of chapters
-chapCount = 1
-author = ''
-title = ''
-finalStory = ''
-notFoundText = 'Chapter not found. Please check to see you are not using an outdated url.' 
-chap = re.get('https://www.fanfiction.net/s/%s/%s/' % (storyId,str(chapCount)))
-
-# Loop till you get notfoundtext.
-# Add all the relevant text to finalStory
-while (html.fromstring(chap.text).xpath('/html/body/div/span/text()[2]')) != notFoundText.split('%'):
-    
-    # Get author and title
-    if title == '':
-        title = html.fromstring(chap.text).xpath('//*[@id="profile_top"]/b')[0].text
-    if author == '':
-        author = html.fromstring(chap.text).xpath('//*[@id="profile_top"]/a[1]')[0].text
-    
-    # Add chapter text to final story
-    finalStory += "<h1>Chapter " + str(chapCount) + "</h1>"
-    finalStory += html.tostring((html.fromstring(chap.text).xpath('//*[@id="storytext"]'))[0])
-
-    print "Chapter " + str(chapCount ) + " found."
-    print "Retreived chapter."
-
-    chapCount += 1
+    # Get number of chapters
+    chapCount = 1
+    author = ''
+    title = ''
+    finalStory = ''
+    notFoundText = 'Chapter not found. Please check to see you are not using an outdated url.' 
     chap = re.get('https://www.fanfiction.net/s/%s/%s/' % (storyId,str(chapCount)))
 
-print chapCount-1
-print title
-print author
+    # Loop till you get notfoundtext.
+    # Add all the relevant text to finalStory
+    while (html.fromstring(chap.text).xpath('/html/body/div/span/text()[2]')) != notFoundText.split('%'):
+        
+        # Get author and title
+        if title == '':
+            title = html.fromstring(chap.text).xpath('//*[@id="profile_top"]/b')[0].text
+        if author == '':
+            author = html.fromstring(chap.text).xpath('//*[@id="profile_top"]/a[1]')[0].text
+        
+        # Add chapter text to final story
+        finalStory += "<h1>Chapter " + str(chapCount) + "</h1>"
+        finalStory += html.tostring((html.fromstring(chap.text).xpath('//*[@id="storytext"]'))[0])
 
-# Open a html file, and put this in the file
-filTitle = title.replace(' ','_')
-htfil = open(filTitle + '.html','w+')
-htfil.write(finalStory)
-htfil.close()
+        print "Chapter " + str(chapCount ) + " found."
 
-# Convert the book using ebook-convert - (Does most of the job for now)
-print "Converting the file......"
-subprocess.call(['ebook-convert',filTitle + '.html',title + '-' + author + '.' + filType])
-subprocess.call(['rm',filTitle + '.html'])
-print "Saved file to " + title + '-' + author + '.' + filType
+        chapCount += 1
+        chap = re.get('https://www.fanfiction.net/s/%s/%s/' % (storyId,str(chapCount)))
+
+    print chapCount-1
+    print title
+    print author
+
+    # Open a html file, and put this in the file
+    filTitle = title.replace(' ','_')
+    htfil = open(filTitle + '.html','w+')
+    htfil.write(finalStory)
+    htfil.close()
+
+    # Convert the book using ebook-convert - (Does most of the job for now)
+    print "Converting the file......"
+    subprocess.call(['ebook-convert',filTitle + '.html',title + '-' + author + '.' + filType])
+    subprocess.call(['rm',filTitle + '.html'])
+    print "Saved file to " + title + '-' + author + '.' + filType
+
+if __name__ == '__main__':
+    if fList == '':
+        getStory(url)
+    else:
+        try:
+            storyList = open(fList,'r')
+        except:
+            print "Error opening the file."
+
+        for line in storyList.readlines():
+            if line != '':
+                print "Started getting story from " + line
+                getStory(line)
+                print "    Moving to the next story \n"
 
